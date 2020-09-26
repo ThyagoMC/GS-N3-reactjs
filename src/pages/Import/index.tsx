286556addcf5174531e150e02a33722d2208314c
Thyago Machado <thymac@gmail.com>
2020-09-26 17:16:46 -0300

    gofinances listagem e upload

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,25 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
+    data.set('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const mappedFiles = files.map(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: `${file.size}`,
+      };
+    });
+    setUploadedFiles(mappedFiles);
   }
 
   return (
